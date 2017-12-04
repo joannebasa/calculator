ea7a0806e3acaa970f192e134d9de0f7e06f274c
Joanne G. Basa <joannegbasa@gmail.com>
2017-12-04 16:15:19 +0000

    registered correct service worker

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import Calculator from './Calculator';
 
 ReactDOM.render(
   <Calculator />,
   document.getElementById('root')
 );
-
-registerServiceWorker();
